910969928d1876346b267571816f4491d8d4acc0
matkatmusic <matkatmusic@gmail.com>
2020-01-12 08:10:00 +0000

    Added part2 instructions

--- main.cpp
+++ main.cpp
@@ -1,31 +1,62 @@
 /*
- Chapter 2 Part 7
- Returning Values tasks
+ Project 4 - Part 2
+ Video: Chapter 3 Part 6
+
+New/This/Pointers/References conclusion
+
+    Build/Run often with this task to make sure you're not breaking the code with each step.
+    I recommend committing after you get each step working so you can revert to a working version easily if needed.
  
- 1) write 3 UDTs named FloatType, DoubleType, IntType.
+ 0) in the blank space below, declare/define an empty struct named 'A' on a single Line. 
+     on the lines below it, write a struct named 'HeapA' that correctly shows how to own an instance of 'A' 
+         on the heap without leaking, without using smart pointers. 
+ */
+
+
+
+
+
+
+
+
+
+
+
+ /*
+ 1) Edit your 3 structs so that they own a heap-allocated primitive type WITHOUT using smart pointers  
+         IntType should own a heap-allocated int, for example.
  
- 2) give each UDT the following member functions:
-        add( lhs, rhs );
-        subtract( lhs, rhs );
-        multiply( lhs, rhs );
-        divide( lhs, rhs );
+ 2) give it a constructor that takes the appropriate primitive
+    this argument will initialize the owned primitive's value.
+         i.e. if you're owning an int on the heap, your ctor argument will initialize that heap-allocated int's value.
  
- 3) make them return the correct primitive type. e.g. if you're implementing the FloatType::add function, your implementation would start like this:
-        float FloatType::add( float lhs, float rhs )
+ 3) modify those add/subtract/divide/multiply member functions from chapter 2 on it
+         a) make them modify the owned numeric type
+         b) set them up so they can be chained together.
+             i.e.
+             DoubleType dt(3.5);
+             dt.add(3.0).multiply(-2.5).divide(7.2); //an example of chaining
  
- 4) declare a few instances of each class in main() and call the member functions with the appropriate arguments.
-        FloatType ft;
-        auto result = ft.add(3.2f, 23.f );
+ 4) write some add/subtract/divide/multiply member functions for each type that take your 3 UDTs
+         for example, IntType::divide(const DoubleType& dt);
  
- 5) print out those results using std::cout:
-        std::cout << "result of ft.add(): " << result << std::endl;
+ 5) print out the results with some creative couts 
+    i.e.
+         FloatType ft(0.1f);
+         IntType it(3);
+         std::cout << "adding 3 and subtracting 'it' from 'ft' results in the following value: " << *ft.add(2.f).subtract( it ).value << std::endl;  //note the dereference of the `value` member of `ft`
  
- 6) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
+ 6) Don't let your heap-allocated owned type leak!
+ 
+ 7) click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
  Wait for my code review.
  */
+
+
+
 
 #include  <stdlib.h>
 #include <iostream>
@@ -159,7 +190,7 @@ int main()
     result = ft.divide( 3.2f, 0.f );
     std::cout << "result of ft.divide(): " << result << std::endl;
 
-    result = dt.add( 3.2, 23.1 );
+    result = dt.add( 3.2, 23.1 ); FIXME result is a float, but dt returns a double.  make another local variable to clear this warning
     std::cout << "result of dt.add(): " << result << std::endl;
     result = dt.subtract( 3.2, 23.1 );
     std::cout << "result of dt.subtract(): " << result << std::endl;
@@ -168,7 +199,7 @@ int main()
     result = dt.divide( 3.2, 0 );
     std::cout << "result of dt.divide(): " << result << std::endl;
 
-    result = it.add( 3, 23 );
+    result = it.add( 3, 23 ); 
     std::cout << "result of it.add(): " << result << std::endl;
     result = it.subtract( 3, 23 );
     std::cout << "result of it.subtract(): " << result << std::endl;
